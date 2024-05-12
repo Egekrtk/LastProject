361b5aba57ad784cc4c5fa4747073c3d393a5cc9
Egekrtk <95716791+Egekrtk@users.noreply.github.com>
2024-05-12 22:11:40 +0300

    Project
    sıkıntı yok

---
+++ Station.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+
+public class Station extends Task{
+    private String stationId;
+    private String capacity;// capacity.length yazdırırız görürüz
+    private String multiFlag;
+    private String fifoFlag;
+    public static ArrayList<String> stationTypesList = new ArrayList<>();
+    private String speed;
+
+
+    public Station(){
+    }
+
+    public Station(String stationId, String capacity, String multiFlag, String fifoFlag, ArrayList<String> taskList,String speed) {
+        super(String.valueOf(taskTypesList=taskList));
+        this.stationId = stationId;
+        this.capacity = capacity;
+        this.multiFlag = multiFlag;
+        this.fifoFlag = fifoFlag;
+        this.speed = speed;
+    }
+
+    public static void createNewStation(String newStationId, String newCapacity, String newMultiflag, String newFifoflag, ArrayList<String> newTaskList, String newSpeed){
+        Station newStation = new Station(newStationId, newCapacity,newMultiflag,newFifoflag , newTaskList,newSpeed);
+        newStation.setStationId(newStationId);
+        stationTypesList.add(newStationId +" ");
+        newStation.toString();
+
+    }
+
+    @Override
+    public String toString() {
+        return "Station{" +
+                "stationId='" + stationId + '\'' +
+                ", capacity='" + capacity + '\'' +
+                ", multiFlag='" + multiFlag + '\'' +
+                ", fifoFlag='" + fifoFlag + '\'' +
+                ", speed=" + speed + '\'' +
+                ", Tasks=" + taskTypesList +
+                '}';
+    }
+
+    public String getStationId() {
+        return stationId;
+    }
+
+    public void setStationId(String stationId) {
+        this.stationId = stationId;
+    }
+
+    public String getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getMultiFlag() {
+        return multiFlag;
+    }
+
+    public void setMultiFlag(String multiFlag) {
+        this.multiFlag = multiFlag;
+    }
+
+    public String getFifoFlag() {
+        return fifoFlag;
+    }
+
+    public void setFifoFlag(String fifoFlag) {
+        this.fifoFlag = fifoFlag;
+    }
+
+    public static ArrayList<String> getStationTypesList() {
+        return stationTypesList;
+    }
+
+    public static void setStationTypesList(ArrayList<String> stationTypesList) {
+        Station.stationTypesList = stationTypesList;
+    }
+
+    public String getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+}
---
+++ Task.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Task {
+    private String taskId;
+    private int taskSize;
+    public static ArrayList<String> taskTypesList = new ArrayList<>();
+    public Task(){
+    }
+
+    public Task(String taskId){
+    this.taskId = taskId;
+    }
+
+    public Task(String taskId, int taskSize){
+        this.taskId = taskId;
+        this.taskSize = taskSize;
+    }
+
+    public static void createNewTask(String taskName, int taskSize){
+        Task newTask = new Task(taskName,taskSize); //yeni oluşturdu
+        newTask.setTaskId(taskName); //girdiğin isme ayarladı
+        String[] tasks = new String[taskSize]; // size ı kadar array için gerekli
+        taskTypesList.add(taskName + " "+taskSize) ;//task typelara ekledi
+
+
+        //System.out.println(newTask.taskId+"ıd"+"  "+newTask.taskSize+"size"+"Oluşturuldu");
+        //System.out.println(taskTypesList+"Task Types list : listeye eklendi");
+        newTask.toString();
+
+        System.out.println("Taskss{");
+        for (int i = 0;i<taskSize;i++){
+            tasks[i] = taskName ;
+            System.out.printf(taskName+" " );
+            //String.valueOf(new String[]{taskName});
+        }
+        System.out.println("}");
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "taskId='" + taskId + '\'' +
+                ", taskSize=" + taskSize +
+                '}';
+    }
+
+
+
+    public String getTaskId() {
+        return taskId;
+    }
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+    public int getTaskSize() {
+        return taskSize;
+    }
+    public void setTaskSize(int taskSize) {
+        this.taskSize = taskSize;
+    }
+    public static ArrayList<String> getTaskList() {
+        return taskTypesList;
+    }
+    public static void setTaskList(ArrayList<String> taskList) {
+        Task.taskTypesList = taskList;
+    }
+}
